8326335e270038f2cba5927ce8c7bced5f08b380
terryhycheng <terryhycheng@gmail.com>
2023-01-26 18:08:21 +0000

    Added functionalities & tests: is-overlapped checker, is-outside-board checker

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -32,19 +32,51 @@ class UserInterface
     end
   end
 
+  def check_isInvalid_position(length, orientation, row, col)
+    if check_isOutside_borad(length, orientation, row, col)
+      show "The ship must be placed inside the borad. Please enter again."
+      return true
+    end
+    
+    if check_isOverlapped(length, orientation, col, row)
+      show "The ship cannot be overlapped. Please enter again."
+      return true
+    end
+    
+    return false
+  end
+  
+  # returns true if ship is overlapped
+  def check_isOverlapped(length, orientation, x, y)
+    if orientation == "v"
+      (y.to_i...(y.to_i + length.to_i)).map{ |each_y| @game.ship_at?(x.to_i, each_y) }.include?(true)
+    else
+      (x.to_i...(x.to_i + length.to_i)).map{ |each_x| @game.ship_at?(each_x, y.to_i) }.include?(true)
+    end
+  end
+  
+  # returns true if it's outside the borad
+  def check_isOutside_borad(length, orientation, row, col)
+    if orientation == "v"
+      @game.cols < col.to_i || @game.rows < (row.to_i + length.to_i)
+    else
+      @game.cols < (col.to_i + length.to_i) || @game.rows < row.to_i
+    end
+  end
+
   def ships_unplaced_message
     return @game.unplaced_ships.map do |ship|
       "#{ship.length}"
     end.join(", ")
   end
 
-  def check_ship_exist(length)
-    return !@game.unplaced_ships.select { |ship| ship.length == length.to_i }.empty?
+  def check_isInvalid_ship(length)
+    return @game.unplaced_ships.select { |ship| ship.length == length.to_i }.empty?
   end
 
   def prompt_for_ship_placement
     ship_length = prompt("Which do you wish to place?", /^\d+$/)
-    while !check_ship_exist(ship_length)
+    while check_isInvalid_ship(ship_length)
       show "There is no ship with the length: #{ship_length}. Please enter again."
       show "You have these ships remaining: #{ships_unplaced_message}"
       ship_length = prompt("Which do you wish to place?", /^\d+$/)
@@ -52,6 +84,10 @@ class UserInterface
     ship_orientation = prompt("Vertical or horizontal? [vh]", /^[vh]$/)
     ship_row = prompt("Which row?", /^\d+$/)
     ship_col = prompt("Which column?", /^\d+$/)
+    while check_isInvalid_position(ship_length, ship_orientation, ship_row, ship_col)
+      ship_row = prompt("Which row?", /^\d+$/)
+      ship_col = prompt("Which column?", /^\d+$/)
+    end
     show "OK."
     @game.place_ship(
       length: ship_length.to_i,
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -5,17 +5,19 @@ RSpec.describe UserInterface do
     it "allows the user to set up ships" do
       io = double(:io)
       game = double(:game, rows: 10, cols: 10)
-      ship = double(:ship, length: 2)
+      ship_2 = double(:ship, length: 2)
+      ship_3 = double(:ship, length: 3)
       interface = UserInterface.new(io, game)
-      expect(ship).to receive(:length).and_return(2)
+      allow(ship_2).to receive(:length).and_return(2)
+      allow(ship_3).to receive(:length).and_return(3)
       expect(io).to receive(:puts).with("Welcome to the game!")
       expect(io).to receive(:puts).with("Set up your ships first.")
-      allow(game).to receive(:unplaced_ships).and_return([ship],[ship],[ship],[ship],[ship],[])
-      expect(io).to receive(:puts).with("You have these ships remaining: 2")
+      allow(game).to receive(:unplaced_ships).and_return([ship_2, ship_3],[ship_2, ship_3],[ship_2, ship_3],[ship_2, ship_3],[ship_2, ship_3],[ship_3], [ship_3], [ship_3], [])
+      expect(io).to receive(:puts).with("You have these ships remaining: 2, 3")
       expect(io).to receive(:puts).with("Which do you wish to place?")
-      expect(io).to receive(:gets).and_return("3\n")
-      expect(io).to receive(:puts).with("There is no ship with the length: 3. Please enter again.")
-      expect(io).to receive(:puts).with("You have these ships remaining: 2")
+      expect(io).to receive(:gets).and_return("5\n")
+      expect(io).to receive(:puts).with("There is no ship with the length: 5. Please enter again.")
+      expect(io).to receive(:puts).with("You have these ships remaining: 2, 3")
       expect(io).to receive(:puts).with("Which do you wish to place?")
       expect(io).to receive(:gets).and_return("2\n")
       expect(io).to receive(:puts).with("Vertical or horizontal? [vh]")
@@ -23,6 +25,13 @@ RSpec.describe UserInterface do
       expect(io).to receive(:puts).with("Invalid input. Please try again.")
       expect(io).to receive(:puts).with("Vertical or horizontal? [vh]")
       expect(io).to receive(:gets).and_return("v\n")
+      expect(io).to receive(:puts).with("Which row?")
+      expect(io).to receive(:gets).and_return("10\n")
+      expect(io).to receive(:puts).with("Which column?")
+      expect(io).to receive(:gets).and_return("2\n")
+      expect(game).to receive(:cols).and_return(10)
+      expect(game).to receive(:rows).and_return(10)
+      expect(io).to receive(:puts).with("The ship must be placed inside the borad. Please enter again.")
       expect(io).to receive(:puts).with("Which row?")
       expect(io).to receive(:gets).and_return("3\n")
       expect(io).to receive(:puts).with("Which column?")
@@ -34,15 +43,39 @@ RSpec.describe UserInterface do
         row: 3,
         col: 2
         })
+      expect(io).to receive(:puts).with("You have these ships remaining: 3")
+      expect(io).to receive(:puts).with("Which do you wish to place?")
+      expect(io).to receive(:gets).and_return("3\n")
+      expect(io).to receive(:puts).with("Vertical or horizontal? [vh]")
+      expect(io).to receive(:gets).and_return("h\n")
+      expect(io).to receive(:puts).with("Which row?")
+      expect(io).to receive(:gets).and_return("4\n")
+      expect(io).to receive(:puts).with("Which column?")
+      expect(io).to receive(:gets).and_return("2\n")
+      expect(io).to receive(:puts).with("The ship cannot be overlapped. Please enter again.")
+      expect(io).to receive(:puts).with("Which row?")
+      expect(io).to receive(:gets).and_return("4\n")
+      expect(io).to receive(:puts).with("Which column?")
+      expect(io).to receive(:gets).and_return("5\n")
+      expect(io).to receive(:puts).with("OK.")
+      expect(game).to receive(:place_ship).with({
+        length: 3,
+        orientation: :horizontal,
+        row: 4,
+        col: 5
+        })
       expect(io).to receive(:puts).with("This is your board now:")
       allow(game).to receive(:ship_at?).and_return(false)
-      allow(game).to receive(:ship_at?).with(2, 3).and_return(true)
-      allow(game).to receive(:ship_at?).with(2, 4).and_return(true)
+      allow(game).to receive(:ship_at?).with(2, 3).and_return(false, true)
+      allow(game).to receive(:ship_at?).with(2, 4).and_return(false, true)
+      allow(game).to receive(:ship_at?).with(5, 4).and_return(false, true)
+      allow(game).to receive(:ship_at?).with(6, 4).and_return(false, true)
+      allow(game).to receive(:ship_at?).with(7, 4).and_return(false, true)
       expect(io).to receive(:puts).with([
         "..........",
         "..........",
         ".S........",
-        ".S........",
+        ".S..SSS...",
         "..........",
         "..........",
         "..........",
